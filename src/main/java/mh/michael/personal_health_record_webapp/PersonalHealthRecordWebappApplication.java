c85bdfe186485c671449a5ccade954becce0a95a
compumike08 <compumike08@users.noreply.github.com>
2025-04-15 18:20:31 -0400

    Add @EnableTransactionManagement annotation

--- src/main/java/mh/michael/personal_health_record_webapp/PersonalHealthRecordWebappApplication.java
+++ src/main/java/mh/michael/personal_health_record_webapp/PersonalHealthRecordWebappApplication.java
@@ -2,8 +2,10 @@ package mh.michael.personal_health_record_webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class PersonalHealthRecordWebappApplication {
 
 	public static void main(String[] args) {

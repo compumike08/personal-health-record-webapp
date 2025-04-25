f1dcdda2fa131dd84760ff985caee9843ec9c82e
compumike08 <compumike08@users.noreply.github.com>
2025-04-24 20:59:37 -0400

    Add bootstrap.css import to index.jsk

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

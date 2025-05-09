49a7a967cb56448219b594ac88381dfec8490eec
compumike08 <compumike08@users.noreply.github.com>
2025-05-09 02:37:39 -0400

    Fix eslint config for typescript

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -4,10 +4,12 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactJsx from "eslint-plugin-react/configs/jsx-runtime.js";
 import react from "eslint-plugin-react/configs/recommended.js";
 import globals from "globals";
+import ts from 'typescript-eslint';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
+  ...ts.configs.recommended,
   ...fixupConfigRules([
     {
       ...react,
@@ -18,19 +20,12 @@ export default [
     reactJsx
   ]),
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     plugins: {
       "react-hooks": reactHooks
     },
     rules: {
       ...reactHooks.configs.recommended.rules
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
     },
   },
   { ignores: ["**/dist/*"] }

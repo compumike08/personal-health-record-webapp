c848a0d28b7ff09a0ef19e54d8e144b1eadca393
compumike08 <compumike08@users.noreply.github.com>
2025-05-11 19:12:52 -0400

    Reset immunizations list reducer when logging out in frontend

--- app/src/features/auth/LogoutPage.tsx
+++ app/src/features/auth/LogoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { resetImmunizationsList } from "../immunizations/immunizationsSlice";
 import { resetMedicationsList } from "../medications/medicationsSlice";
 import { resetPatientsData } from "../patients/patientsSlice";
 import { resetAuthData } from "./authSlice";
@@ -12,6 +13,7 @@ const LogoutPage = () => {
   useEffect(() => {
     dispatch(resetPatientsData());
     dispatch(resetMedicationsList());
+    dispatch(resetImmunizationsList());
     dispatch(resetAuthData());
     void navigate("/");
   }, [dispatch, navigate]);
